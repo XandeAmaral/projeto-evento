433e479a5c84fdbb2f376c788a2df242968400ad
Alexandre Amaral <alexandrecamaral22@gmail.com>
2023-05-29 21:08:23 -0300

    arrumando banco

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $integrante = new Integrante();
 $tela = "telaIntegrante";
 $msg = "";
 
-if (!empty(filter_input(INPUT_GET,'msg'))) {
+if (!empty(filter_input(INPUT_GET, 'msg'))) {
   $msg = filter_input(INPUT_GET, 'msg');
   $dados = $integrante->consultarUltimo();
 
@@ -12,7 +12,7 @@ if (!empty(filter_input(INPUT_GET,'msg'))) {
     $id = $dados[0]['id_int'];
     $integrante->setId($id);
 
-    if(!empty($id)){
+    if (!empty($id)) {
       $dados = $integrante->consultarPorID();
 
       if (is_array($dados) && !empty($dados)) {
@@ -22,17 +22,14 @@ if (!empty(filter_input(INPUT_GET,'msg'))) {
         $nome = $linha['nome_int'];
         $data = $linha['data_int'];
         $cpf = $linha['cpf_int'];
-      }
-      else
+      } else
         echo '<div> Falha no retorno do método de buscar por id <div/>';
-    }
-    else echo "<div> falha ao tentar retornar o usuario aslvo <div/>";
-  }
-  else
+    } else echo "<div> falha ao tentar retornar o usuario aslvo <div/>";
+  } else
     echo '<div> Falha no retorno do método de buscar por id <div/>';
 }
 
-if (!empty(filter_input(INPUT_GET,'id'))) {
+if (!empty(filter_input(INPUT_GET, 'id'))) {
   $id = filter_input(INPUT_GET, 'id');
   $integrante->setId($id);
   $dados = $integrante->consultarPorID();
@@ -45,12 +42,10 @@ if (!empty(filter_input(INPUT_GET,'id'))) {
     $nome = $linha['nome_int'];
     $data = $linha['data_int'];
     $cpf = $linha['cpf_int'];
-  }
-  else
+  } else
     echo '<div> Falha no retorno do método de buscar por id <div/>';
-}
-else {
-  if (!empty(filter_input(INPUT_GET,'cpf'))) {
+} else {
+  if (!empty(filter_input(INPUT_GET, 'cpf'))) {
     echo 'entrei com cpf';
     $cpf = filter_input(INPUT_GET, 'cpf');
     $integrante->setCpf($cpf);
@@ -71,36 +66,39 @@ else {
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-	</head>
-	<body>
-		<h1>Cadastro de integrante</h1>
-		<form method="post">
-      <b>ID: </b>
-      <input type="text" placeholder="Insira o ID" name="txtId" value="<?= isset($id) ? $id : "" ?>"/><br/><br/>
-			<b>Personagem: </b>
-			<input type="text" placeholder="Insira o nome do personagem" name="txtPersonagem" value="<?= isset($personagem) ? $personagem : "" ?>"/><br/><br/>
-			<b>Nome: </b>
-			<input type="text" placeholder="Insira o nome do participante" name="txtNome" value="<?= isset($nome) ? $nome : "" ?>"/><br/><br/>
-			<b>Data: </b>
-			<input type="text" placeholder="Insira a data" name="txtData" value="<?= isset($data) ? $data : "" ?>"/><br/><br/>
-			<b>CPF: </b>
-			<input type="text" placeholder="Insira o cpf" name="txtCpf" value="<?= isset($cpf) ? $cpf : ""?>"/><br/><br/>
-			<input type="submit" name="btnCadastroIntegrante" value="Cadastrar Integrante">
-      <input type="submit" name="btnBuscarId" value="Buscar por ID">
-      <input type="submit" name="btnBuscarCPF" value="Buscar por CPF">
-      <?php
-        if (!empty($msg)) {
-          echo "<br/><br/><div>$msg!!</div>";
-        }
-      ?>
-		</form>
-	</body>
+
+<head>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <h1>Cadastro de integrante</h1>
+  <form method="post">
+    <b>ID: </b>
+    <input type="text" placeholder="Insira o ID" name="txtId" value="<?= isset($id) ? $id : "" ?>" /><br /><br />
+    <b>Personagem: </b>
+    <input type="text" placeholder="Insira o nome do personagem" name="txtPersonagem" value="<?= isset($personagem) ? $personagem : "" ?>" /><br /><br />
+    <b>Nome: </b>
+    <input type="text" placeholder="Insira o nome do participante" name="txtNome" value="<?= isset($nome) ? $nome : "" ?>" /><br /><br />
+    <b>Data: </b>
+    <input type="text" placeholder="Insira a data" name="txtData" value="<?= isset($data) ? $data : "" ?>" /><br /><br />
+    <b>CPF: </b>
+    <input type="text" placeholder="Insira o cpf" name="txtCpf" value="<?= isset($cpf) ? $cpf : "" ?>" /><br /><br />
+    <input type="submit" name="btnCadastroIntegrante" value="Cadastrar Integrante">
+    <input type="submit" name="btnBuscarId" value="Buscar por ID">
+    <input type="submit" name="btnBuscarCPF" value="Buscar por CPF">
+    <?php
+    if (!empty($msg)) {
+      echo "<br/><br/><div>$msg!!</div>";
+    }
+    ?>
+  </form>
+</body>
+
 </html>
 
 <?php
-if (filter_input(INPUT_POST, 'btnCadastroIntegrante')){
+if (filter_input(INPUT_POST, 'btnCadastroIntegrante')) {
   $id = filter_input(INPUT_POST, 'txtId');
   $personagem = filter_input(INPUT_POST, 'txtPersonagem');
   $nome = filter_input(INPUT_POST, 'txtNome');
@@ -117,16 +115,16 @@ if (filter_input(INPUT_POST, 'btnCadastroIntegrante')){
   $integrante->setCpf($cpf);
   $msg = $integrante->salvar();
 
-  header("Location: http://localhost/teste/?p=$tela&msg=$msg");
+  header("Location: http://localhost/site%20evento/?p=$tela&msg=$msg");
   exit();
 }
 if (filter_input(INPUT_POST, 'btnBuscarId')) {
   $id = filter_input(INPUT_POST, 'txtId');
-  header("Location: http://localhost/teste/?p=$tela&id=$id");
+  header("Location:http://localhost/site%20evento/?p=$tela&id=$id");
   exit();
 }
-if(filter_input(INPUT_POST,'btnBuscarCPF')){
+if (filter_input(INPUT_POST, 'btnBuscarCPF')) {
   $cpf = filter_input(INPUT_POST, 'txtCpf');
-  header("Location: http://localhost/teste/?p=$tela&cpf=$cpf");
+  header("Location: http://localhost/site%20evento/?p=$tela&cpf=$cpf");
   exit();
 }
